16f2e25f1021ae03b1c5da81f34d3ab25a00a39e
Piotr Wlazeł <pancu1222@gmail.com>
2019-09-23 12:22:50 +0200

    Add script build for production and change webpack config to function to build env

---
+++ src/Containers/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import style from "./App.css";
+import uuid from "uuid";
+import Title from "../Components/Title";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { text: "one", id: "1" },
+        { text: "one", id: "2" },
+        { text: "one", id: "3" }
+      ]
+    };
+  }
+  addTodo(val) {
+    const todo = {
+      text: val,
+      id: uuid.v4()
+    };
+    const data = [...this.state.data, todo];
+    this.setState({ data });
+  }
+  removeTodo(id) {
+    const remainder = this.state.data.filter(todo => todo.id !== id);
+    this.setState({ data: remainder });
+  }
+  render() {
+    return (
+      <div className={style.TodoApp}>
+        <Title title={"My Todo App"} tasks={this.state.data.length} />
+      </div>
+    );
+  }
+}
+
+export default App;
